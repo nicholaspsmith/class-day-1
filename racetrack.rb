152b1e25d336ed0cf715022b105bd5c972b7a552
MakerSquare Student <students@themakersquare.com>
2013-09-09 17:19:02 -0500

    Visually show car positions, allow placing of bets, no payout yet

--- racetrack.rb
+++ racetrack.rb
@@ -5,7 +5,7 @@
 # 
 
 class Racecar
-	attr_accessor :speed, :name
+	attr_accessor :speed, :name, :position
 
 	def initialize name
 		@name = name
@@ -26,7 +26,8 @@ class Racetrack
 		@time = 0
 		@cars = Array.new
 		@race_start = false
-		prompt_user
+		#prompt_user
+		#start
 	end
 
 	def prompt_user
@@ -36,24 +37,37 @@ class Racetrack
 			user_input = gets.chomp
 			addCar(user_input)
 		end
-		start# race
 	end
 
-	def addCar(name)
-		if (!@race_start and name != "start")
-			@cars.push(Racecar.new(name))
+	# def addCar(car)
+	# 	if (!@race_start and name != "start")
+	# 		@cars << car
+	# 	end
+	# end
+
+	def addCar(car)
+		if (!@race_start)
+			@cars << car
 		end
 	end
 
 	def start
 		@race_start = true
+
 		@cars.each do |x|
 			##give each car a random speed between (60..80)
 			x.speed= rand(60..80)
 		end
 
+		@cars.sort! { |a,b| b.speed <=> a.speed }
+
 		begin
-			puts "---------------\nLap #{@time+1}\n---------------"
+			puts "******************\nLap #{@time+1}\n******************"
+			@cars.each do |x|
+				puts "#{x.name}: #{@cars.index(x)+1}"
+				x.speed.times { print "-"}
+				puts ">"
+			end
 			forward
 			place_bets
 		end while (@time < 5)
@@ -68,8 +82,11 @@ class Racetrack
 
 	def forward# @time by one hour
 		@cars.each do |car|
-			car.increase_speed rand(21)# number between 0..20
+			car.increase_speed rand(0..21)# number between 0..20
 		end
+
+		@cars.sort! { |a,b| b.speed <=> a.speed }
+
 		@time += 1
 	end
 
@@ -95,10 +112,16 @@ class Racetrack
 		@cars.each do |car|
 			puts car.name
 		end
+
 		ans = gets.chomp
+
 		@cars.each do |car|
 			if car.name == ans
 				puts "Bet #{amt} on #{car.name}"
+
+				##get car position
+				puts "#{car.name} is in #{@cars.index(car)+1}"
+				##payout = 1 * car position
 			end
 		end
 		
@@ -108,3 +131,15 @@ class Racetrack
 		puts "print_payout to be implemented..."
 	end
 end
+
+mms = Racecar.new("Danica")
+michelin = Racecar.new("Dale")
+dupont = Racecar.new("Jeff")
+daytona = Racetrack.new()
+daytona.addCar(mms)
+daytona.addCar(michelin)
+daytona.addCar(dupont)
+
+daytona.start
+
+
